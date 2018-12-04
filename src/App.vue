f193c0895ff2d8a2759bc46955921d8e3f11dee1
Gong zhiyu <1607341178@qq.com>
2018-12-04 09:24:19 +0800

    fist commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <!--显示当前路由组件-->
+    <router-view/>  <!--路由组件-->
+    <!--普通组件-->
+    <FooterGuide v-show="$route.meta.FooterGuide"/>    <!--标识底部显示-->
+  </div>
+</template>
+
+<script>
+  import FooterGuide from './components/FooterGuide/FooterGuide'
+
+  export default {
+     mounted() {
+    //异步获取地址数据(到vuex的state中)
+      this.$store.dispatch('getAddress')
+      //异步获取取商铺列表
+      this.$store.dispatch('getShops')
+      //异步获取当前用户信息
+      this.$store.dispatch('getUser')
+    },
+    name: 'App',
+    components: {
+      FooterGuide,
+    }
+  }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus" scoped>
+
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,18 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import store from './store'
+import './mock/mockServer'
+import './filters/index'
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  components: { App },
+  template: '<App/>',
+  router,
+  store,  //每个组件都有一个$store属性
+})
